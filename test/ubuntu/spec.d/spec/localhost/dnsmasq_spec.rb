f633b143ad222ef9ce849d269ff6a65b534eca60
Andreas Schmidt <andreas.schmidt@cassini.de>
2014-08-27 12:35:47 +0200

    removed listen spec because dnsmasq is unconfigured for dhcp at boot time

--- test/ubuntu/spec.d/spec/localhost/dnsmasq_spec.rb
+++ test/ubuntu/spec.d/spec/localhost/dnsmasq_spec.rb
@@ -13,11 +13,6 @@ describe 'It should have dnsmasq installed' do
   describe process 'dnsmasq' do
     it { should be_running }
   end
-
-  describe port 67 do
-    it { should be_listening.with(:udp) }
-  end
-
 end
 
 
